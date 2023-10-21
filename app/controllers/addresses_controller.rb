b467f09c00fabc12b339e2f278c727e301a250d9
kajigii <wata15708@gmail.com>
2023-10-22 02:23:11 +0900

    検索結果表示機能実装

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,26 @@
 class AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+  end
+
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+  private
+
+  def address_params
+    params.require(:address).permit(:departure, :destination_1, :destination_2).merge(user_id: current_user.id)
   end
 end
